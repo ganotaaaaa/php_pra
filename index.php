13a3838b49d53eede4ae1d7c896549ad85ce3f8c
MYCOMPUTER\jakap <ys.training210629@gmail.com>
2024-04-04 11:18:31 +0900

    add form

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<h2>hello</h2>
+<form action="<?php echo $_SERVER['REQUEST_URI']?>" method="post">
+    <input type="text" name="todo">
+    <input type="submit" value="追加">
+</form>
+
+<ul>
+    <?php echo "<li>{$_POST['todo']}<li>" ?>
+</ul>
+
